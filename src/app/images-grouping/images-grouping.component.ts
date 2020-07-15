b6ce5771e8034023814c35ba89814c9a39ab1cb3
Vladimir Svoboda <svoboda-vlad@seznam.cz>
2020-07-15 10:43:58 +0000

    Added loaded files names list

--- src/app/images-grouping/images-grouping.component.ts
+++ src/app/images-grouping/images-grouping.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImagesGroupingComponent implements OnInit {
 
+  fileNames: string[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  processFiles(files: FileList): void {
+    if(files.length > 0) {
+      this.fileNames = this.getFileNames(files);
+    }
+  }
+
+  getFileNames(files: FileList): string[] {
+    let fileNames: string[] = [];
+    for (let i = 0; i < files.length; i++) {
+      fileNames[i] = files.item(i).name;
+    }
+    return fileNames;
   }
 
 }
