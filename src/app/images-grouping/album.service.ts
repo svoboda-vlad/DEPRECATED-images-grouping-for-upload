0bae86b1be2eaa3ee937f1c65c7c47f4acfa89de
Vladimir Svoboda <svoboda-vlad@seznam.cz>
2020-10-23 08:15:02 +0000

    handleerror method adjusted

--- src/app/images-grouping/album.service.ts
+++ src/app/images-grouping/album.service.ts
@@ -28,18 +28,19 @@ export class AlbumService {
     .pipe(catchError(error => this.handleError(error))).toPromise();
   }
 
-  /** Error handler */
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
-      // A client-side error.
-      console.error('An client-side error occurred:', error.error.message);
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
     } else {
-      // The backend error.
-      console.error('An backend error occurred:', error.error.message);
-      return throwError(error);
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
     }
-    // return a custom error message
-    return throwError('Something bad happened; please try again later.');
+    // Return an observable with a user-facing error message.
+    return throwError(error);
   }
 
 }
