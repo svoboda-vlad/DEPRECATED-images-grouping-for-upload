928f379937265b1b878347275ff6771902d86b48
Vladimir Svoboda <svoboda-vlad@seznam.cz>
2020-11-06 09:20:50 +0000

    different client id for test and prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  GOOGLE_CLIENT_ID: '401460954055-6enk5fknh1gs1nfkpo0pf03orovdb3vm.apps.googleusercontent.com'
 };
 
 /*
